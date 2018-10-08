9bc264a18d0e97db3d158765efb72a20a0f161e6
Sofia Berglund <berglund.sofia@gmail.com>
2018-10-08 16:49:37 +0200

    fixed footer alignment
    social media icons were outside of footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,30 +7,30 @@ export default class Footer extends React.Component {
 
   render() {
 
-      return (
-        <footer>
+    return (
+      <footer>
 
-          <div className="footer-logo">
-            <img src="/images/logo.png"/>
+        <div className="footer-logo">
+          <img src="/images/logo.png"/>
+        </div>
+        <div className="footer-navbar">
+          <ul>
+            <li>&#169; 2014-2018 Comprend</li>
+            <li>Privacy Notice</li>
+            <li>Cookie Policy</li>
+            <li>Terms of Use</li>
+            <li>Subscribe to our newsletter</li>
+          </ul>
+          <div className="socialmedia-icons">
+            <img src="/images/facebook.png" />
+            <img src="/images/twitter.png" />
+            <img src="/images/instagram.png" />
+            <img src="/images/linkedin.png" />
           </div>
-          <div className="footer-navbar">
-            <ul>
-              <li>&#169; 2014-2018 Comprend</li>
-              <li>Privacy Notice</li>
-              <li>Cookie Policy</li>
-              <li>Terms of Use</li>
-              <li>Subscribe to our newsletter</li>
-            </ul>
-            <div className="socialmedia-icons">
-              <img src="/images/facebook.png" />
-              <img src="/images/twitter.png" />
-              <img src="/images/instagram.png" />
-              <img src="/images/linkedin.png" />
-            </div>
-          </div>
-            <p>Comprend is part H&H Group</p>
-           
-        </footer>
-      )
-    }
+        </div>
+          <p>Comprend is part H&H Group</p>
+
+      </footer>
+    )
+  }
 }
