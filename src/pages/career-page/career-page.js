5d6505fdf08ef4c810b45c7f1cb34e6bbacc78ce
Naomi <naomi.tesfay@gmail.com>
2018-10-10 15:36:36 +0200

    Career Page
    Added a second picture & button

--- src/pages/career-page/career-page.js
+++ src/pages/career-page/career-page.js
@@ -6,31 +6,46 @@ class CareerPage extends React.Component {
   render() {
     return (
 
-<div>
+<div className="career-container">
       <div className="hero-image">
-      <img src="images/yoda.jpg" />
-      <div className="hero-text">
-      <h1>Join us</h1>
-        <p>May the force be with you</p>
-        </div>
+        <img src="images/yoda.jpg" />
+          <div className="hero-text">
+          <h1>Join us</h1>
+          <p>May the force be with you</p>
+          </div>
+      </div>
+
+      <div className="intro-container">
+          <div className="intro-text">
+              <h2>This is Comprend</h2>
+              <p> Lorem ipsum dolor sit amet, putant voluptua vix ea. Impedit commune vix te. Id case mucius molestie mea, quod congue regione pro ea. Id dicat dicta incorrupte nec, vel in tollit aliquip.</p>
+          </div>
+          <div className="intro-text">
+              <h2>This is Comprend</h2>
+              <p> Lorem ipsum dolor sit amet, putant voluptua vix ea. Impedit commune vix te. Id case mucius molestie mea, quod congue regione pro ea. Id dicat dicta incorrupte nec, vel in tollit aliquip.</p>
+          </div>
+          <div className="intro-text">
+            <h2>This is Comprend</h2>
+            <p> Lorem ipsum dolor sit amet, putant voluptua vix ea. Impedit commune vix te. Id case mucius molestie mea, quod congue regione pro ea. Id dicat dicta incorrupte nec, vel in tollit aliquip.</p>
+          </div>
         </div>
 
-      <div className="intro-text">
-          <h2>Lorem ipsum dolor sit amet</h2>
-          <p> Lorem ipsum dolor sit amet, putant voluptua vix ea. Impedit commune vix te. Id case mucius molestie mea, quod congue regione pro ea. Id dicat dicta incorrupte nec, vel in tollit aliquip.</p>
-          <h2>Lorem ipsum dolor sit amet</h2>
-          <p> Lorem ipsum dolor sit amet, putant voluptua vix ea. Impedit commune vix te. Id case mucius molestie mea, quod congue regione pro ea. Id dicat dicta incorrupte nec, vel in tollit aliquip.</p>
-          <h2>Lorem ipsum dolor sit amet</h2>
-          <p> Lorem ipsum dolor sit amet, putant voluptua vix ea. Impedit commune vix te. Id case mucius molestie mea, quod congue regione pro ea. Id dicat dicta incorrupte nec, vel in tollit aliquip.</p>
+
+
+        <div className="job-opening-container">
+          <img src="images/last-jedi.jpg" />
+          <div className="button-on-image">
+          <button>Job openings</button>
+          </div>
+        </div>
+
       </div>
 
 
 
-        <div className="Colleague">
 
-        </div>
 
-</div>
+
 
     )
   }
