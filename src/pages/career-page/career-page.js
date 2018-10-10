225604337c8d41a10129900c620c9ca8ad8a8e50
Naomi <naomi.tesfay@gmail.com>
2018-10-10 12:07:50 +0200

    career page - added hero image + intro text

--- src/pages/career-page/career-page.js
+++ src/pages/career-page/career-page.js
@@ -7,14 +7,22 @@ class CareerPage extends React.Component {
     return (
 
 <div>
-      <div className="hero">
-      <img src="" />
-      <p>Join us</p>
+      <div className="hero-image">
+      <img src="images/yoda.jpg" />
+      <div className="hero-text">
+      <h1>Join us</h1>
+        <p>May the force be with you</p>
+        </div>
         </div>
 
-        <div className="intro-text">
-
-        </div>
+      <div className="intro-text">
+          <h2>Lorem ipsum dolor sit amet</h2>
+          <p> Lorem ipsum dolor sit amet, putant voluptua vix ea. Impedit commune vix te. Id case mucius molestie mea, quod congue regione pro ea. Id dicat dicta incorrupte nec, vel in tollit aliquip.</p>
+          <h2>Lorem ipsum dolor sit amet</h2>
+          <p> Lorem ipsum dolor sit amet, putant voluptua vix ea. Impedit commune vix te. Id case mucius molestie mea, quod congue regione pro ea. Id dicat dicta incorrupte nec, vel in tollit aliquip.</p>
+          <h2>Lorem ipsum dolor sit amet</h2>
+          <p> Lorem ipsum dolor sit amet, putant voluptua vix ea. Impedit commune vix te. Id case mucius molestie mea, quod congue regione pro ea. Id dicat dicta incorrupte nec, vel in tollit aliquip.</p>
+      </div>
 
 
 
