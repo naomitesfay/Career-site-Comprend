99bdf5c47e686294a9fe5ef7b7cd24321eea5c2a
kblanchfield <kateblanchfield@live.co.uk>
2018-10-11 22:34:23 +0200

    Hover effect for colleagues

--- src/components/colleague/colleague.js
+++ src/components/colleague/colleague.js
@@ -3,15 +3,22 @@ import "./colleague.css"
 
 export default class Colleague extends React.Component {
 
+  reserveFaceUrls = [
+                    "./images/face-1.png",
+                     "./images/face-2.png",
+                     "./images/face-3.png",
+                     "./images/face-4.png",
+                     "./images/face-5.png",
+                     "./images/face-6.png"
+                   ]
+
   render() {
-
-    const onMouseEnter = {onMouseEnter}
-    const onMouseLeave = {onMouseLeave}
-
       return (
-        <div className="colleague-block" onMouseEnter={this.props.onMouseEnter}
-        onMouseLeave={this.props.onMouseLeave}>
-          <img className="colleague-image" src={this.props.source} alt="" />
+        <div className="colleague-block" >
+          <div className="image-block">
+            <img className="colleague-image" src={this.props.source} alt={this.props.name} />
+            <img className="colleague-image-sw" src={this.reserveFaceUrls[Math.floor(Math.random()*6)]} />
+          </div>
           <p className="colleague-name">{this.props.name}</p>
           <p className="colleague-title">{this.props.title}</p>
         </div>
--- src/pages/colleagues-page/colleagues-page.js
+++ src/pages/colleagues-page/colleagues-page.js
@@ -5,66 +5,31 @@ import "./colleagues-page.css"
 class ColleaguesPage extends React.Component {
 
   state = {
-    allColleagues: [],
-    source: "https://res.cloudinary.com/teamtailor/image/upload/c_fill,dpr_2.0,f_auto,g_faces:center,h_160,q_80,w_160/v1456739836/ydy7jbqal3izyiiy4pir.jpg"
+    allColleagues: []
   }
-
-  reserveFaceUrls = ["./images/face-1.png",
-                     "./images/face-2.png",
-                     "./images/face-3.png",
-                     "./images/face-4.png",
-                     "./images/face-5.png",
-                     "./images/face-6.png",
-                  ]
 
   componentDidMount() {
     fetch("http://hellotechnigo.comprendwebsites.net/api/users").then((response) => {
       return response.json()
     }).then((json) => {
-      this.setState({
-        allColleagues: json,
-      })
-    })
-    }
-
-    handleHoverOn = () => {
-      this.setState({
-        source: "/images/face-1.png"
-      })
-    }
-
-    handleHoverOff = () => {
-      this.setState({
-        source: "https://res.cloudinary.com/teamtailor/image/upload/c_fill,dpr_2.0,f_auto,g_faces:center,h_160,q_80,w_160/v1456739836/ydy7jbqal3izyiiy4pir.jpg"
-      })
-    }
+          this.setState({
+            allColleagues: json
+            })
+        })
+      }
 
   render() {
     if (this.state.allColleagues.length > 0) {
-      {this.state.allColleagues.map((person) => {
-        if(!person.pictureUrl) {
-          person.pictureUrl = this.reserveFaceUrls[Math.floor(Math.random()*6)]
-        }
-      })}
       return (
        <div className="background">
           <div className="colleagues-container">
-          {/*
           {this.state.allColleagues.map((person) => {
-            if (person.visible) {
-              return <Colleague source={person.pictureUrl}
-                  name={person.name}
-                  title={person.title}
-                  onMouseEnter={this.handleHover} />
-              }
-          })}
-          */}
-          <Colleague source={this.state.source}
-                   name={this.state.allColleagues[0].name}
-                   title={this.state.allColleagues[0].title}
-                   onMouseEnter={this.handleHoverOn}
-                   onMouseLeave={this.handleHoverOff}
-                   />
+             if (person.visible) {
+               return <Colleague source={person.pictureUrl}
+                   name={person.name}
+                   title={person.title} />
+               }
+           })}
           </div>
        </div>
       )
